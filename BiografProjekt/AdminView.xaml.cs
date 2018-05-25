18f13708a4748bab0009b0e1bd2dc6e7eee26c6a
MrH4mm3r <mr_h4mm3r@live.dk>
2018-05-25 17:22:44 +0200

    Updated AdminView

--- BiografProjekt/AdminView.xaml.cs
+++ BiografProjekt/AdminView.xaml.cs
@@ -1,29 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BiografProjekt
 {
     /// <summary>
     /// Interaction logic for AdminView.xaml
     /// </summary>
-    public partial class AdminView : Page
+    public partial class AdminView
     {
         public AdminView()
         {
             InitializeComponent();
+
+            ComboboxHour.Items.Add(0);
+            for (int i = 1; i < 24; i++)
+            {
+                ComboboxHour.Items.Add(i);
+            }
+
+            for (int i = 0; i < 56; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    ComboboxMinute.Items.Add(i);
+                }
+            }
         }
 
         private void ButtonBase_OnClick__(object sender, RoutedEventArgs e)
